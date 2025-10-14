a6a9881bf330b1b2eb08c48c80d867d77007fc61
Kirti1120 <shelkekirti45@gmail.com>
2025-10-14 17:31:49 +0000

    datatype summary

---
+++ 001_basics/001_variables.js
@@ -0,0 +1,14 @@
+const accountId = 145627
+let accountEmail = "kirti@google.com"
+var accountPassword = "24556"
+accountCity = "Jaipur"
+
+console.log(accountEmail);
+accountEmail="kir@vhc.com"
+ accountPassword="3648327"
+accountCity="Kolhapur"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+/*
+dont use "var" becoz of issue in block scope and fumctional scope
+*/
--- 001_basics/03_conversionOp.js
+++ 001_basics/03_conversionOp.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
+console.log(typeof score);  //string
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+console.log(typeof valueInNumber);  //number
+console.log(valueInNumber);         //NaN
 
 
 //"33" easily converted to=>33
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);   //true
 
 // 1 => true, 0 => false
 // ""=> false
@@ -23,6 +23,33 @@ console.log(booleanIsLoggedIn);
 let someNum = 33
 
 let stingNumber = String(someNum)
-console.log(stingNumber);
-console.log(typeof stingNumber);
+console.log(stingNumber);         //33
+console.log(typeof stingNumber);  //string
+
+//***************OPERATIONS***************//
+
+let value =3 
+let negValue = -value
+console.log(negValue);     //-33
+
+//Basics Operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "kirti"
+let str3 = str1+str2
+console.log(str3);    //hellokirti
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2);  //122
+// console.log(1 + 2 + "2");  //1+2=3 therefore 32
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
 

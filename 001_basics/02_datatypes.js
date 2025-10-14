5f3a3bcb0574617ddf8efc6289220b09f539cbbe
Kirti1120 <shelkekirti45@gmail.com>
2025-10-14 16:17:09 +0000

    conversion of datatypes

---
+++ 001_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3)   //we are using node.js , not the browser
+
+console.log("kirti")
+
+let name="kirti"
+let age=18
+let isLoggedIn=false
+
+
+//number=>2 to power 53
+//bigint
+//string=> ""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=> unique
+
+//object
+console.log(typeof "kirti");    //output= string
+console.log(typeof age);        //output= number
+console.log(typeof null);       //output= object
+console.log(typeof undefined);  //output= undefined
+
---
+++ 001_basics/03_conversionOp.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" easily converted to=>33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// ""=> false
+// "kirti" => true
+
+let someNum = 33
+
+let stingNumber = String(someNum)
+console.log(stingNumber);
+console.log(typeof stingNumber);
+

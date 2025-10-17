146549f58bafcaa98a10ca88705bcf5cb2208dcf
Kirti1120 <shelkekirti45@gmail.com>
2025-10-17 18:17:23 +0000

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,17 @@
+const month = "march"
+
+switch(month){
+    case "jan" :
+        console.log("January");
+        break;
+    case "feb" :
+        console.log("Feb");
+        break;
+    case "march" :
+        console.log("March");
+        break;
+    default : 
+    console.log("Default case ,match");
+    break;
+    
+}                                       //March

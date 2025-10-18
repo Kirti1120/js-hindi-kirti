9eb07b6aa1507381b22860ef47351d5d4dce1cba
Kirti1120 <shelkekirti45@gmail.com>
2025-10-18 08:49:37 +0000

    while and do while loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,27 @@
+// while and do ehile loop
+
+let i = 0
+while(i<=5){
+   console.log(`Value of i is ${i}`);        
+//Value of i is 0 
+// Value of i is 2
+// Value of i is 4
+    i = i+2                       
+    
+}
+ 
+//do while loop   (ya madhe kam pahila hotay ani condition nanter check hotay)
+
+let score = 1
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+    
+}while(score <= 5)
+
+// Score is 1
+// Score is 2
+// Score is 3
+// Score is 4
+// Score is 5
